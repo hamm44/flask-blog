6c0614ad8510da6f2e68d4119c0ab117d1f900f0
hamm44 <mrbighit@gmail.com>
2014-09-23 22:27:18 +1000

    decorator

--- blog.py
+++ blog.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, request, session, \
 	flash, redirect, url_for, g
 
 import sqlite3
+from functools import wraps
+
 
 # configuration
 DATABASE = 'blog.db'
@@ -21,6 +23,16 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, *kwargs)
+		else:
+			flash('You need to login first')
+			return redirect(url_for('login'))
+		return wrap
+
 @app.route('/', methods = ['GET', 'POST'])
 def login():
 	error = None
@@ -35,6 +47,7 @@ def login():
 
 
 @app.route('/main')
+@login_required
 def main():
 	return render_template('main.html')
 
